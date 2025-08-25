4c1e86e4e547e0d44191817b034b54183ba31d9a
Aslam-Naseer <aslamnaseer22@gmail.com>
2025-08-25 15:17:25 +0530

    Add animation to all direction movement

--- SFML_RPG/Player.cpp
+++ SFML_RPG/Player.cpp
@@ -12,7 +12,7 @@ Player::Player(float x, float y, sf::Texture& texture)
 	//initComponents();
 	createMovementComponent(700.f, 25.f, 5.f);
 	createAnimationComponent(texture);
-	createHitboxComponent(85, 55, 75, 135);
+	createHitboxComponent(95, 55, 80, 135);
 
 	setPosition(x, y);
 	animationComponent->addAnimation("IDLE", 192, 192, 14, 0, .5f);
@@ -29,9 +29,29 @@ void Player::update(const float& dt)
 	movementComponent->update(dt);
 
 	if (movementComponent->getState(MovementComponent::movement_state::WALK_LEFT))
+	{
+		sprite.setOrigin({ 0.f, 0 });
+		sprite.setScale({ 1.f, 1.f });
 		animationComponent->play("WALK");
-	else
+	}
+	else if (movementComponent->getState(MovementComponent::movement_state::WALK_RIGHT))
+	{
+		sprite.setOrigin({ 258.f, 0 });
+		sprite.setScale({ -1.f, 1.f });
+		animationComponent->play("WALK");
+	} 
+	else if(movementComponent->getState(MovementComponent::movement_state::WALK_UP))
+	{
+		animationComponent->play("WALK");
+	} 
+	else if (movementComponent->getState(MovementComponent::movement_state::WALK_DOWN))
+	{
+		animationComponent->play("WALK");
+	} 
+	else 
+	{
 		animationComponent->play("IDLE");
+	}
 
 	animationComponent->update(dt);
 	hitboxComponent->update();
